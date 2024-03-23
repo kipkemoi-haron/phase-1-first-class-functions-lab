4e65e82f8d6fe6a153fda56c0f8cbdc13e942efe
kipkemoi Haron <kipkemoiharon4@gmail.com>
2024-03-23 23:09:13 +0300

    complete lab

--- index.js
+++ index.js
@@ -1,1 +1,38 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(drivers) {
+    return drivers.slice(0, 2);
+  }
+  
+  function returnLastTwoDrivers(drivers) {
+    return drivers.slice(-2);
+  }
+  
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  
+  function createFareMultiplier(multiplier) {
+    return function fareMultiplier(fare) {
+      return fare * multiplier;
+    };
+  }
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  function selectDifferentDrivers(drivers, selectingFunction) {
+    return selectingFunction(drivers);
+  }
+  
+  module.exports = {
+    returnFirstTwoDrivers,
+    returnLastTwoDrivers,
+    selectingDrivers,
+    createFareMultiplier,
+    fareDoubler,
+    fareTripler,
+    selectDifferentDrivers
+  };
+  
